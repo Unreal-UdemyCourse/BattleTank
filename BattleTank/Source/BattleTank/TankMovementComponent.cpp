f9c02869de5b5c5ad53a1f0bbb5d0cf8c287970b
Diefleder <the_laughing_bandit@hotmail.com>
2019-01-30 08:40:54 -0800

    S04L168 Completing Manual Tank Movement

--- BattleTank/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -1,12 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
 
 
 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	float Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f Throttle at %f"), Time, Throw)
+	if (CheckTrackPointers())
+	{
+		LeftTrack->SetThrottle(Throw);
+		RightTrack->SetThrottle(Throw);
+	}
+}
+
+void UTankMovementComponent::IntendMoveRight(float Throw)
+{
+	if (CheckTrackPointers())
+	{
+		LeftTrack->SetThrottle(Throw);
+		RightTrack->SetThrottle(-Throw);
+		UE_LOG(LogTemp, Warning, TEXT("Throttle at %f"), Throw)
+	}
+}
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
+
+bool UTankMovementComponent::CheckTrackPointers()
+{
+	// Handle null pointer
+	if (LeftTrack == nullptr || RightTrack == nullptr) { return false; }
+	else { return true; }
 }
--- BattleTank/Source/BattleTank/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/TankMovementComponent.h
@@ -6,10 +6,13 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+
+class UTankTrack;
+
 /**
- * 
+ *  Responsible for driving the tank tracks
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
@@ -17,6 +20,18 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 public:
 	UFUNCTION(BlueprintCallable)
 	void IntendMoveForward(float Throw);
-	
-	
+
+	UFUNCTION(BlueprintCallable)
+	void IntendMoveRight(float Throw);
+
+	UFUNCTION(BlueprintCallable)
+	void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
+	//Check tank tracks for null pointer
+	bool CheckTrackPointers();
+
+
+private:
+	UTankTrack*  LeftTrack = nullptr;
+	UTankTrack*  RightTrack = nullptr;
 };
